eee4be836d94a831f566b05791e46c067dc84b8b
jawda <jawdat.abdullah@innovisec.com>
2022-09-13 00:40:35 +0300

    using array

--- script.js
+++ script.js
@@ -12,228 +12,185 @@ const operators = ['+', '-', '*', '/'];
 
 //#region methods
 const initElements = () => {
-    resultContent = document.querySelector(".result-container__result-label");
-    numbersButtons = document.querySelectorAll(".calculator-numbers__num-btn");
-    opertaorsButtons = document.querySelectorAll(".calculator-operator__ctrl-btn");
-    operationElement = document.querySelector(".result-container__operation-string");
-    resetCalculator();
-    lastOperator = '+';
+  resultContent = document.querySelector(".result-container__result-label");
+  numbersButtons = document.querySelectorAll(".calculator-numbers__num-btn");
+  opertaorsButtons = document.querySelectorAll(".calculator-operator__ctrl-btn");
+  operationElement = document.querySelector(".result-container__operation-string");
+  resetCalculator();
+  lastOperator = '+';
 }
 
 const resetCalculator = () => {
-    result = 0;
-    operationElement.textContent = "";
-    operationValue = "";
+  result = 0;
+  operationElement.textContent = "";
+  operationValue = "";
 }
 
 const updateTextContent = () => {
-    resultContent.textContent = `Result: ${result}`;
+  resultContent.textContent = `Result: ${result}`;
 }
 
 const calculateOpertaion = () => {
-    let copy = operationValue || '';
+  let copy = operationValue || '';
 
-    // todo: change the code to use array list instead of while loop
-    while (strContainsOperation(copy)) {
-        const idxOfMultiply = copy.indexOf('*');
-        const idxOfDevider = copy.indexOf('/');
+  let operationSentence = getOperationSentenceElements(copy);
 
-        if (idxOfMultiply > -1 || idxOfDevider > -1) {
-            let firstIdx = -1;
+  while (arrContainsOperation(operationSentence)) {
+    const firstPriorityIdxs = operationSentence.map((char, idx) => {
+      if (char === '*' || char === '/') {
+        return idx;
+      }
+    }).filter(idx => idx);
+    const lastPriorityIdxs = operationSentence.map((char, idx) => {
+      if (char === '+' || char === '-') {
+        return idx;
+      }
+    }).filter(idx => idx);
 
-            if ((idxOfMultiply > -1 && idxOfDevider > -1) && idxOfMultiply < idxOfDevider) { firstIdx = idxOfMultiply }
-            else if ((idxOfMultiply > -1 && idxOfDevider > -1) && idxOfDevider < idxOfMultiply) { firstIdx = idxOfDevider }
-            else if ((idxOfMultiply > -1 && idxOfDevider === -1)) { firstIdx = idxOfMultiply }
-            else if ((idxOfMultiply === -1 && idxOfDevider > -1)) { firstIdx = idxOfDevider }
+    if (firstPriorityIdxs.length > 0) {
+      let calculatedResult = calculateOperandOpenand(
+        operationSentence[firstPriorityIdxs[0] - 1],
+        operationSentence[firstPriorityIdxs[0] + 1],
+        operationSentence[firstPriorityIdxs[0]]);
 
-
-            const partOne = copy.slice(0, firstIdx);
-            const partTwo = copy.slice(firstIdx + 1, copy.length);
-            const operation = copy[firstIdx];
-
-            let partOneNumbers = getNumbersFromOperationString(partOne);
-            let partTwoNumbers = getNumbersFromOperationString(partTwo);
-
-            const calculate = calculateOperandOpenand(partOneNumbers[partOneNumbers.length - 1], partTwoNumbers[0], operation);
-
-            partOneNumbers.pop();
-            partTwoNumbers.shift();
-
-            const newPartOne = generateNewPart(partOne, true);
-            const newPartTwo = generateNewPart(partTwo, false);
-
-            copy = newPartOne + calculate + newPartTwo;
-            operationElement.textContent = copy;
-        } else {
-            firstIdx = -1;
-            const idxOfPlus = copy.indexOf('+');
-            const idxOfMinus = copy.indexOf('-');
-            if ((idxOfPlus > -1 && idxOfMinus > -1) && idxOfPlus < idxOfMinus) { firstIdx = idxOfPlus }
-            else if ((idxOfPlus > -1 && idxOfMinus > -1) && idxOfMinus < idxOfPlus) { firstIdx = idxOfMinus }
-            else if ((idxOfPlus > -1 && idxOfMinus === -1)) { firstIdx = idxOfPlus }
-            else if ((idxOfPlus === -1 && idxOfMinus > -1)) { firstIdx = idxOfMinus }
-
-
-            const partOne = copy.slice(0, firstIdx);
-            const partTwo = copy.slice(firstIdx + 1, copy.length);
-            const operation = copy[firstIdx];
-
-            let partOneNumbers = getNumbersFromOperationString(partOne);
-            let partTwoNumbers = getNumbersFromOperationString(partTwo);
-
-            const calculate = calculateOperandOpenand(partOneNumbers[partOneNumbers.length - 1], partTwoNumbers[0], operation);
-
-            partOneNumbers.pop();
-            partTwoNumbers.shift();
-
-            const newPartOne = generateNewPart(partOne, true);
-            const newPartTwo = generateNewPart(partTwo, false);
-
-            copy = newPartOne + calculate + newPartTwo;
-            operationElement.textContent = copy;
+      let newSentance = [];
+      for (let idx = 0; idx < operationSentence.length; idx++) {
+        const element = operationSentence[idx];
+        if (idx !== +firstPriorityIdxs - 1 && idx !== +firstPriorityIdxs && idx !== +firstPriorityIdxs + 1) {
+          newSentance.push(element);
         }
+        if (idx === +firstPriorityIdxs - 1) {
+          newSentance.push(`${calculatedResult}`);
+        }
+      }
+      operationSentence = newSentance;
+    } else {
+      calculatedResult = calculateOperandOpenand(
+        operationSentence[lastPriorityIdxs[0] - 1],
+        operationSentence[lastPriorityIdxs[0] + 1],
+        operationSentence[lastPriorityIdxs[0]]);
+
+      newSentance = [];
+      for (let idx = 0; idx < operationSentence.length; idx++) {
+        const element = operationSentence[idx];
+        console.log(element)
+        if (idx !== +lastPriorityIdxs - 1 && idx !== +lastPriorityIdxs && idx !== +lastPriorityIdxs + 1) {
+          newSentance.push(element);
+        }
+        if (idx === +lastPriorityIdxs - 1) {
+          newSentance.push(`${calculatedResult}`);
+        }
+      }
+      operationSentence = newSentance;
     }
-    result = copy;
-    updateTextContent();
+  }
+  result = operationSentence;
+  updateTextContent();
+}
+
+const getOperationSentenceElements = (operationSentance) => {
+  let number = '';
+  const sentanceElements = [];
+  [...operationSentance].forEach((char, idx) => {
+    if (idx === [...operationSentance].length - 1) {
+      number += char;
+      sentanceElements.push(number);
+    }
+    if (operators.indexOf(char) === -1) {
+      number += char;
+    } else {
+      if (number !== '') {
+        sentanceElements.push(number);
+        number = '';
+      }
+      sentanceElements.push(char);
+    }
+  });
+
+  return sentanceElements;
 }
 
 const calculateOperandOpenand = (operand, openand, operation) => {
-    let value = 0;
-    switch (operation) {
-        case '+':
-            value = +operand + +openand;
-            break;
-        case '-':
-            value = +operand - +openand;
-            break;
-        case '*':
-            value = +operand * +openand;
-            break;
-        case '/':
-            value = +operand / +openand;
-            break;
+  let value = 0;
+  switch (operation) {
+    case '+':
+      value = +operand + +openand;
+      break;
+    case '-':
+      value = +operand - +openand;
+      break;
+    case '*':
+      value = +operand * +openand;
+      break;
+    case '/':
+      value = +operand / +openand;
+      break;
 
-    }
-    return value;
+  }
+  return value;
 }
 
-const strContainsOperation = (str) => {
-    for (let i = 0; i < str.length; i++) {
-        const element = str[i];
-        if (operators.indexOf(element) > -1) {
-            return true;
-        }
+const arrContainsOperation = (arr) => {
+  if (arr && arr.length) {
+    for (e of arr) {
+      if (operators.indexOf(e) > -1) {
+        return true;
+      }
     }
-    return false;
-}
-
-const generateNewPart = (str, isFirst) => {
-    newStr = '';
-    if (isFirst) {
-        newStr = str.split("").reverse().join("");
-        console.log(newStr)
-        let lastOpIdx = -1;
-        for (let i = 0; i < newStr.length && lastOpIdx === -1; i++) {
-            if (operators.indexOf(newStr[i]) > -1) {
-                lastOpIdx = i;
-            }
-        }
-        if (lastOpIdx > -1) {
-            newStr = newStr.slice(lastOpIdx, newStr.length);
-            newStr = newStr.split("").reverse().join("");
-        } else {
-            return "";
-        }
-    } else {
-        newStr = str;
-        let firstOpIdx = -1;
-        for (let i = 0; i < newStr.length && firstOpIdx === -1; i++) {
-            if (operators.indexOf(newStr[i]) > -1) {
-                firstOpIdx = i;
-            }
-        }
-
-        if (firstOpIdx > -1) {
-            newStr = newStr.slice(firstOpIdx, newStr.length);
-        } else {
-            return "";
-        }
-    }
-    return newStr;
-}
-
-const getNumbersFromOperationString = (str) => {
-    let lastNumber = '';
-    const numbersArr = [];
-    for (let i = 0; i < str.length; i++) {
-        if (i === str.length - 1) {
-            lastNumber += str[i];
-            numbersArr.push(lastNumber);
-            lastNumber = '';
-        } else {
-            if (operators.indexOf(str[i]) === -1) {
-                lastNumber += str[i];
-            } else {
-                numbersArr.push(lastNumber);
-                lastNumber = '';
-            }
-        }
-    }
-    return numbersArr;
+  }
+  return false;
 }
 //#endregion methods
 
 
 //main method
 
-const init = () => {
-    initElements();
+document.addEventListener("DOMContentLoaded", () => {
+  initElements();
 
-    updateTextContent();
+  updateTextContent();
 
-    numbersButtons.forEach(btn => {
-        btn.addEventListener("click", (e) => {
-            if (!operationValue) {
-                operationValue = '';
-            }
-            operationValue += e.target.textContent;
-            operationElement.textContent = operationValue;
-        });
+  numbersButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      if (!operationValue) {
+        operationValue = '';
+      }
+      operationValue += e.target.textContent;
+      operationElement.textContent = operationValue;
     });
+  });
 
-    opertaorsButtons.forEach(btn => {
-        btn.addEventListener("click", (e) => {
-            if (operationValue) {
-                const invalidInput = operators.indexOf(operationValue[operationValue.length - 1]) > -1;
-                switch (e.target.textContent) {
-                    case 'AC':
-                        resetCalculator();
-                        updateTextContent();
-                        break;
-                    case '=':
-                        if (invalidInput) {
-                            operationValue = operationValue.slice(0, operationValue.length - 1);
-                            operationElement.textContent = operationValue;
-                        }
-                        calculateOpertaion();
-                        updateTextContent();
-                        break;
-                    default:
-                        const newValue = e.target.textContent;
-
-
-                        if (!invalidInput) {
-                            operationValue += newValue;
-                            operationElement.textContent = operationValue;
-                        }
-                        break;
-                }
-                lastOperator = e.target.textContent;
-
+  opertaorsButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      if (operationValue) {
+        const invalidInput = operators.indexOf(operationValue[operationValue.length - 1]) > -1;
+        switch (e.target.textContent) {
+          case 'AC':
+            resetCalculator();
+            updateTextContent();
+            break;
+          case '=':
+            if (invalidInput) {
+              operationValue = operationValue.slice(0, operationValue.length - 1);
+              operationElement.textContent = operationValue;
             }
-        });
-    })
+            calculateOpertaion();
+            updateTextContent();
+            break;
+          default:
+            const newValue = e.target.textContent;
 
-}
 
-document.addEventListener("DOMContentLoaded", init);
+            if (!invalidInput) {
+              operationValue += newValue;
+              operationElement.textContent = operationValue;
+            }
+            break;
+        }
+        lastOperator = e.target.textContent;
+
+      }
+    });
+  })
+
+})
